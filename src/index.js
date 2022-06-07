0e2c27a8fe1b92c8c31c34087d4489d2180b94bc
Manya7864 <manishyadav78642080@gmail.com>
2022-06-08 01:42:22 +0530

    figma

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../src/component/Figma.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from './context/ThemeContext';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChakraProvider>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
